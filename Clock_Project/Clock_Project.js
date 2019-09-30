d6c77ba54f1d04ac2c97dbc3e260a964cb03acca
srobi79@lsu.edu <iTunes1D>
2019-09-30 10:12:00 -0500

    update
    update

--- Clock_Project/Clock_Project.js
+++ Clock_Project/Clock_Project.js
@@ -1,16 +1,30 @@
+let bg;
+
+function preload() {
+    pumpkin = loadImage('Robichaux_Sophia_ART2210-1/Clock_Project/pumpkin.jpg')
+}
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 }
 
 function draw() {
-  background(0);
-
-   let hr = hour();
-   let min = minute();
-   let sc = second();
+  background(255);
+  Image(pumpkin 300,300)
+  textSize(20);
   
-  fill(255);
-  noStroke();
-  text(hr + ':' + mn + ':' + sc, 10,  200);
+  //space numbers around a circle
+  for(var i=1; i<13; i++){
+    text(i, width/2 +sin(i/6*PI)*150,height/2-cos(i/6*PI)*150);
+  }
   
+  //add hour hand
+  strokeWeight(6);
+  stroke(255, 204, 0);
+  line(width/2, height/2, width/2 +sin((hour()+minute()/60)/6*PI)*50,height/2-cos((hour()+minute()/60)/6*PI)*50);
+  //add minute hand
+  strokeWeight(4);
+  line(width/2, height/2, width/2 +sin(minute()/30*PI)*75,height/2-cos(minute()/30*PI)*75);
+  //add second hand
+  strokeWeight(2);
+  line(width/2, height/2, width/2 +sin(second()/30*PI)*100,height/2-cos(second()/30*PI)*100);
 }
