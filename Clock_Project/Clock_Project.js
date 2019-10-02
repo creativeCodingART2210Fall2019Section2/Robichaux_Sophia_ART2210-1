612a965e84e4ae83bed00eb1b940d3028939e5c7
srobi79@lsu.edu <iTunes1D>
2019-10-02 10:14:07 -0500

    Update Clock_Project.js

--- Clock_Project/Clock_Project.js
+++ Clock_Project/Clock_Project.js
@@ -1,43 +1,37 @@
-
 function setup() {
   createCanvas(600, 600);
+//The "colorMode" function changes the way the code interprets color, allowing me to do cool stuff like changing the color.
   colorMode(HSB,360,100,100);
 }
 
-function draw() 
-{
+function draw(){
+///STYLING OF THE CLOCK
   background(20);
-   ellipse(300, 300, 100, 100);
-  
-  var sec = second();
-  var More = second() + 1;
-  
-  for (var sec = 0; sec <= More; sec += 1){
-    fill(random(255), random(255), random(255));
-  }
+//These makes up the circles
+   circle(300, 300, 100);
+   circle(300, 300, 70);
+   circle(300, 300, 20);
+   circle(70, 70, 40);
+   circle(500,200,100);
+   circle(200,500,45)
+//This fills the circles and numbers with the random color  
+{ fill(random(255), random(255), random(255)); }
 
   textSize(20);
 
-  
-  //Number Spaces
+//This tells the computer how to space out the numbers
   for(var i=1; i<13; i++){
     text(i, width/2 +sin(i/6*PI)*150,height/2-cos(i/6*PI)*150);
   }
-  circle(300, 300, 70);
-  circle(300, 300, 20);
- 
-  //Hour
+///ACTUAL CLOCK CODE
+//Hour
   strokeWeight(6);
   stroke(255, 204, 0);
   line(width/2, height/2, width/2 +sin((hour()+minute()/60)/6*PI)*50,height/2-cos((hour()+minute()/60)/6*PI)*50);
-  //Minute
+//Minute
   strokeWeight(4);
   line(width/2, height/2, width/2 +sin(minute()/30*PI)*75,height/2-cos(minute()/30*PI)*75);
-  //Second
+//Second
   strokeWeight(2);
   line(width/2, height/2, width/2 +sin(second()/30*PI)*100,height/2-cos(second()/30*PI)*100);
-
-  function newFunction() {
-    image(i, this.x, this.y);
-  }
 }
