984ccfc564f5845480da445db9ae7333620eeb4f
srobi79@lsu.edu <iTunes1D>
2019-12-03 23:37:00 -0600

    update

---
+++ Exercise4_Json_Data/README.md
@@ -0,0 +1,2 @@
+## Robichaux_Sophia_ART2210
+[CLICK HERE FOR MY FIRST JSON!](https://sophiarobichaux.github.io/Robichaux_Sophia_ART2210-1//Exercise4_Json_Data/index.html)
---
+++ Exercise4_Json_Data/movies.js
@@ -0,0 +1,28 @@
+var list;
+
+function preload(){
+  list =loadJSON("movies.json");
+}
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+  background(155,55,5);
+  textAlign(LEFT);
+  a = int(random(0,list.movies.length));
+}
+
+function draw(){
+  background(155,55,5);
+  stroke(4);
+  textSize(25);
+  push();
+  textAlign(RIGHT);
+  text(list.movies[a].name,width/2-125,height/2)
+  pop();
+}
+
+  function windowResized(){
+    resizeCanvas(windowWidth,windowHeight); 
+    background(155,55,5 );
+
+
+}
